32bd0b5cbadc89f4ee4a7575289fa9e770739c51
Mark Conroy <mark@mark.ie>
2020-01-06 21:23:15 +0000

    Deploying PatternLab

--- patternlab.sh
+++ patternlab.sh
@@ -1,5 +1,5 @@
-scp -r ../mark.ie/web/themes/custom/patternlab/dest ~/htdocs/adesignforlife/mark.ie-patternlab
-scp -r ../mark.ie/web/themes/custom/patternlab/pattern-lab ~/htdocs/adesignforlife/mark.ie-patternlab
+scp -r ../markie/web/themes/custom/patternlab/dest ~/htdocs/adesignforlife/mark.ie-patternlab
+scp -r ../markie/web/themes/custom/patternlab/pattern-lab ~/htdocs/adesignforlife/mark.ie-patternlab
 git add -A
 git commit -m "Deploying PatternLab"
 git push
